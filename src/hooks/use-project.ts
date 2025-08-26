420ddf5fdf088cbb73cf9f07b82bfd133076347e
tanvimandhan <tanvimandhan34@gmail.com>
2025-08-26 17:56:23 +0530

    add window fix

--- src/hooks/use-project.ts
+++ src/hooks/use-project.ts
@@ -1,16 +1,19 @@
 import { api } from '@/trpc/react'
-import {useLocalStorage} from 'usehooks-ts'
+import { useLocalStorage } from 'usehooks-ts'
 
 function useProject() {
-  const {data:projects}=api.project.getProjects.useQuery()
-  const [projectId,setProjectId]=useLocalStorage('codesage','')
-  const project=projects?.find(project=>project.id ===projectId)
+  const { data: projects } = api.project.getProjects.useQuery()
+
+  const isClient = typeof window !== "undefined"
+  const [projectId, setProjectId] = isClient ? useLocalStorage('codesage', '') : [null, () => {}]
+
+  const project = projects?.find((project) => project.id === projectId)
 
   return {
     projects,
     project,
     setProjectId,
-    projectId
+    projectId,
   }
 }
 
